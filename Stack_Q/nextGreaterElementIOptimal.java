52b2b4eb42e34a81ea15573030fb8568474ccccd
Darshanbelokar <belokardarshan676@gmail.com>
2026-08-04 19:16:56 +0530

    Next Greater Element optimal solution

---
+++ Stack_Q/nextGreaterElementIOptimal.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+public class nextGreaterElementIOptimal {
+  public static void main(String[] args) {
+    
+  }
+
+  public static int[] nextGreaterElement(int[] nums1,int[] nums2){
+    Stack<Integer> result = new Stack<Integer>();
+
+    int n= nums.length;
+  }
+}
