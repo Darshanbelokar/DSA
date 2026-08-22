0c9d264bbca3d55edf86d886ba1cd132bf1d0e36
Darshanbelokar <belokardarshan676@gmail.com>
2026-08-22 13:22:20 +0530

    Added Math Folder

---
+++ Math/checkDivisibilitybuSUmProduct.java
@@ -0,0 +1,22 @@
+public  class checkDivisibilitybuSUmProduct{
+  public static void main(String[] args) {
+    int m = 232;
+    System.out.println(checkDivisibility(m));
+  }
+
+  public static boolean checkDivisibility(int n){
+    String nums = Integer.toString(n);
+
+    int sum = 0;
+    int product = 1;
+
+    for(char num : nums.toCharArray()){
+      int digit = num - '0';
+      sum += num;
+
+      product *= num;
+    }
+    
+    return sum+product == n;
+  }
+}
