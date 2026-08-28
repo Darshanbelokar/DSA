66f0baf98d23f81db73848d5631bd64e0d143dbd
Darshanbelokar <belokardarshan676@gmail.com>
2026-08-28 11:24:55 +0530

    index of first occurence in a string

---
+++ String/indexoFirstOccurenceinString.java
@@ -0,0 +1,23 @@
+public class indexoFirstOccurenceinString {
+  public static void main(String[] args) {
+    String haystack = "sadbutsad";
+    String needle = "sad";
+    System.out.println(strStr(haystack,needle));
+  }
+
+  public static int strStr(String haystack,String needle){
+
+    for(int i = 0;i < haystack.length() - needle.length();i++){
+      int j;
+      for(j = 0; j < needle.length();j++){
+        if(haystack.charAt(i + j) != needle.charAt(j)){
+          break;
+        }
+      }
+      if(j == needle.length()){
+        return i;
+      }
+    }
+    return -1;
+  }
+}
